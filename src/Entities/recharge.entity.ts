8765673c766ce0937bba386655ab4bc08c402510
sam-zarila <zarilatroxx99@gmail.com>
2025-02-25 12:51:18 -0800

    recharge entity

---
+++ src/Entities/recharge.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+
+export class rechargeEntity{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    serviceType: 'escom' | 'waterboard';
+
+    @Column()
+    accountIdentifier: string;
+
+    @Column('decimal', { precision:10, scale:2 })
+    amount: number;
+
+    @Column('decimal', { precision:10, scale:2 })
+    units: number;
+
+    @Column()
+    token: string;
+
+    @Column({type:'timestamp', default: () => 'CURRENT_TIMESTAMP'})
+    rechargeDate:Date
+}
