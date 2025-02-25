169f58e7a53b44e80969bf5c46a2f85b8ca0f6a5
sam-zarila <zarilatroxx99@gmail.com>
2025-02-25 12:40:54 -0800

    custom recharges api

---
+++ src/DTO/Recharge.DTO.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger"
+
+//recharge DTO for handling user input
+
+export class rechargeDTO{
+
+    @ApiProperty()
+    serviceType: 'escom' | 'waterboard'
+
+    @ApiProperty()
+    firebaseUid: string
+
+    @ApiProperty()
+    accountIdentifier?: string
+
+    @ApiProperty()
+    amount: number
+
+
+}
