32651bcdd9dd209051010bc1e16a2e0efbb9f47a
sam-zarila <zarilatroxx99@gmail.com>
2025-02-25 16:33:18 -0800

    retrieving units based on amount

--- src/custom_recharges/controllers/custom_recharges/custom_recharges.controller.ts
+++ src/custom_recharges/controllers/custom_recharges/custom_recharges.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CustomRechargesService } from '../../services/custom_recharges/custom_recharges.service';
 import { rechargeDTO } from 'src/DTO/Recharge.DTO';
@@ -17,6 +17,17 @@ export class CustomRechargesController {
     async processRecharge(@Body() dto: rechargeDTO) {
         return this.CustomRechargesService.processRecharge(dto);
        
+    }
+    @Get('units')
+    @ApiOperation({summary: 'Calculate units'})
+
+    getUnits(
+        @Query('serviceType') serviceType: 'escom' | 'waterboard',
+        @Query('amount') amount: number
+    ){
+        const units = this.CustomRechargesService.calculateUnits(serviceType, Number(amount));
+        return {serviceType,amount,units}
+
     }
 }
 
