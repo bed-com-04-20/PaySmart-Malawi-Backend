a53e7b4e8e265f95445ee4dbe38e762ba69893d0
sam-zarila <zarilatroxx99@gmail.com>
2025-02-26 15:08:27 -0800

    payment is done

--- src/payment_gateway/payment_gateway.module.ts
+++ src/payment_gateway/payment_gateway.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { PaymentGatewayController } from './controller/payment_gateway/payment_gateway.controller';
 import { PaymentWayService } from './service/payment_way/payment_way.service';
 import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RechargeEntity } from 'src/Entities/recharge.entity';
 
 @Module({
-  imports:[HttpModule],
+  imports:[ TypeOrmModule.forFeature([RechargeEntity]),
+    HttpModule],
   controllers: [PaymentGatewayController],
   providers: [PaymentWayService]
 })
