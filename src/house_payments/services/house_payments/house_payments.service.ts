3ab520fee84badfd537c3e16d63d2699929c15c0
sam-zarila <zarilatroxx99@gmail.com>
2025-03-02 04:47:20 -0800

    startting house payments

--- src/house_payments/services/house_payments/house_payments.service.ts
+++ src/house_payments/services/house_payments/house_payments.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { verify } from 'crypto';
+import { houseEntity } from 'src/Entities/House.Entity';
+import { housePaymentEntity } from 'src/Entities/house_payments.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class HousePaymentsService {}
+export class HousePaymentsService {
+    constructor (@InjectRepository(houseEntity)
+    private readonly housepaymentsRepository: Repository<houseEntity>,
+
+    @InjectRepository(housePaymentEntity)
+
+    private readonly housePaymentRepository: Repository<housePaymentEntity>){
+
+       
+    }
+    async verifyPayment(tx_ref:string):Promise<void>{
+        try {
+            const response = await axios.get(
+               `https://api.paychangu.com/verify-payment/${tx_ref}`,
+               {
+                headers: {
+                    Authorization: `Bearer ${process.env.PAYCHANGU_API_KEY}`,
+                   
+                },
+               } 
+            );
+          
+        } catch (error) {
+            
+        }
+    }
+}
