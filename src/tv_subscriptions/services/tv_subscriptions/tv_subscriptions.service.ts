38a42e178164a07adc37b93cee4098b15b492f9e
sam-zarila <zarilatroxx99@gmail.com>
2025-03-12 12:43:55 -0800

    tv subscriptions

--- src/tv_subscriptions/services/tv_subscriptions/tv_subscriptions.service.ts
+++ src/tv_subscriptions/services/tv_subscriptions/tv_subscriptions.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TvPackageEntity } from 'src/Entities/TVpackages.entity';
+import { TVsubscription } from 'src/Entities/TVsubscription.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TvSubscriptionsService {}
+export class TvSubscriptionsService {
+
+    constructor(
+        @InjectRepository(TVsubscription)
+        private readonly tvSubscriptionRepository: Repository<TVsubscription>,
+
+        @InjectRepository(TvPackageEntity)
+        private readonly tvPackageRepository: Repository<TvPackageEntity>
+    ){}
+    async listPackages(serviceId:number){
+        return this.tvPackageRepository.find({ where:{id:serviceId}});
+    }
+}
