8842a55b601f73d7959b93b81d63b61f422ce26c
sam-zarila <zarilatroxx99@gmail.com>
2025-03-12 12:32:25 -0800

    tv subscriptions

--- src/Entities/TVsubscription.entity.ts
+++ src/Entities/TVsubscription.entity.ts
@@ -1,6 +1,25 @@
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { TvPackageEntity } from "./TVpackages.entity";
 
 @Entity()
 export class TVsubscription {
-    // ...
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToMany(()=> TvPackageEntity)
+    packages: TvPackageEntity[];
+
+    @Column()
+    accountNumber: string;
+
+    @Column()
+    tx_ref: string;
+
+    @Column()
+    status:string
+
+    @CreateDateColumn()
+    created_at: Date;
+
+
 }
