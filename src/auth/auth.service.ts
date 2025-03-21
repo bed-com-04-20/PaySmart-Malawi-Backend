e71b683da13a57d8fa090b00801472fe7a937d14
sam-zarila <zarilatroxx99@gmail.com>
2025-03-21 13:26:00 -0800

    user management

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import * as firebaseAdmin from 'firebase-admin';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  async registerUser(registerUser: RegisterUserDto) {
+    console.log(registerUser);
+    try {
+      const userRecord = await firebaseAdmin.auth().createUser({
+        displayName: registerUser.firstName,
+        email: registerUser.email,
+        password: registerUser.password,
+      });
+      console.log('User Record:', userRecord);
+      return userRecord;
+    } catch (error) {
+      console.error('Error creating user:', error);
+      throw new Error('User registration failed'); // Handle errors gracefully
+    }
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
 }
