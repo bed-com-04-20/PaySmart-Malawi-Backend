62f4421c2f123b731a2c95a0d528df8ae90360f3
bed-com-04-20 <bed-com-04-20@unima.ac.mw>
2025-03-23 10:41:48 -0700

    database configurations changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,14 +26,15 @@ import { User } from './user/entities/user.entity';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost', // Change if using a remote database
-      port: 5432, // Default PostgreSQL port
-      username: 'postgres',
-      password: 'tech-nest265',
-      database: 'paysmart_malawi',
-      entities: [RechargeEntity,HousePayment,InstallmentPayment,TVServiceEntity,TvPackageEntity,TVsubscription, User ],
-      synchronize: true, // Set to false in production
+      host: process.env.DB_HOST, 
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: false, // or true in dev
+      autoLoadEntities: true,
     }),
+    
 
 
     // TypeOrmModule.forRoot({
